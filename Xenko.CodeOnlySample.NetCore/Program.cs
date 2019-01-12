5ea631d5facc9fb5f06a2a59b0f42f790dffdc46
Virgile Bello <virgile.bello@gmail.com>
2019-01-12 15:01:10 +0900

    Added .NET Core executable

---
+++ Xenko.CodeOnlySample.NetCore/Program.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Xenko contributors (https://xenko.com)
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+namespace Xenko.CodeOnlySample.NetCore
+{
+    class Xenko_CodeOnlySampleApp
+    {
+        static void Main(string[] args)
+        {
+            using (var game = new CodeOnlyGame())
+            {
+                game.Run();
+            }
+        }
+    }
+}
